118fd31e7d6f66e34539549c73386640a46f5e1a
OLayemii <olayemi.garuba@algorismng.com>
2019-07-30 12:56:58 +0100

    feat: Added the header component to the layouts directory
    The header component takes a title props which defaults to the site title
    and an optional subtitle prop which will be used in single post view

---
+++ src/components/layouts/header/Header.js
@@ -0,0 +1,28 @@
+import React from "react";
+import Proptypes from "prop-types";
+
+import "./Header.css";
+
+function Header(props) {
+  let { title, subtitle } = props;
+  return (
+    <div className="Header">
+      <header className="Header">
+        <h1>{title}</h1>
+        {subtitle && <h5>{subtitle}</h5>}
+      </header>
+    </div>
+  );
+}
+
+Header.propTypes = {
+  title: Proptypes.string.isRequired,
+  subtitle: Proptypes.string
+};
+
+Header.defaultProps = {
+  title: "Epower Blog",
+  subtitle: ""
+};
+
+export default Header;
