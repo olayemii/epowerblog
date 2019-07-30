6f82566918ce392a1fbbdcbbcc91293eafe8a512
OLayemii <olayemi.garuba@algorismng.com>
2019-07-30 18:44:38 +0100

    feat: Added post view and single post view
    The views to handle a post and a singe post on clicking the post card

--- src/views/post/Post.js
+++ src/views/post/Post.js
@@ -1,11 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { MainLayout } from "../../components";
-
+import axios from "axios";
+import "./Post.css";
 function Post(props) {
-  let { post } = props;
+  const [currPost, setCurrPost] = useState(null);
+  useEffect(() => getPostBySlug(props.match.params.postid), []);
+
+  const getPostBySlug = slug => {
+    setCurrPost(null);
+    axios
+      .get(`https://epower.ng/wp-json/wp/v2/posts/?slug=${slug}`)
+      .then(res => {
+        setCurrPost(res.data[0]);
+        console.log(res);
+      })
+      .catch(err => alert("Error fetching content"));
+  };
   return (
     <div className="Post">
-      <MainLayout>{post}</MainLayout>
+      <MainLayout
+        headerTitle={
+          currPost ? currPost.title && currPost.title.rendered : "Loading..."
+        }
+        headerSubTitle={
+          currPost ? "PUBLISHED ON " + currPost.post_date : "Loading..."
+        }
+      >
+        {currPost ? (
+          currPost.content && (
+            <div
+              className="content"
+              dangerouslySetInnerHTML={{ __html: currPost.content.rendered }}
+            />
+          )
+        ) : (
+          <div>Loading. . .</div>
+        )}
+      </MainLayout>
     </div>
   );
 }
